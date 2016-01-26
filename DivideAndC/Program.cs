25a69e27d9028f53fa681f5bbc76acbb09c2e5ea
Dave Mateer <davemateer@gmail.com>
2016-01-26 06:08:07 +0000

    simple recursion

--- DivideAndC/Program.cs
+++ DivideAndC/Program.cs
@@ -9,31 +9,43 @@ namespace DivideAndC
     {
         static void Main()
         {
-            // generate numbers with a Divide and Conquer approach
-            // but not using recursive.  It is possible:
-            // http://stackoverflow.com/questions/29974792/does-any-divide-and-conquer-algorithm-use-recursion
-
-            //int[] input3 = { 22, 4, 6, 0, 9, 12, 156, 86, 99 };
-            //MergeSort(input3, 0, input3.Length - 1);
-            //for (int i = 0; i < input3.Length; i++)
-            //    Console.Write(input3[i] + ", ");
-            //Console.WriteLine("");
+            // 1,2,3,4,5,6
+            var numbers = Enumerable.Range(1, 6).ToList();
+            List<int> result = DivCon(numbers);
+            foreach (var number in result) Console.WriteLine(number);
         }
 
-        private List<int> DivideUp(int maxInt)
+        private static List<int> DivCon(List<int> numbers)
         {
-            // http://yangliucsharp.blogspot.co.uk/2013/02/divide-and-conquer-algorithm.html
-            var result = new List<int>();
-            var list = Enumerable.Range(1, maxInt).ToList();
+            if (numbers.Count > 0)
+            {
+                var midwayPosition = (numbers.Count/2)-1;
+                var midway = numbers[midwayPosition];
+                Console.WriteLine(midway);
 
-            int minIndex = 0;
-            int maxIndex = list.Count - 1;
-            result.Add(list[0]); // the list.first is always the first
-            result.Add(list[maxIndex]); // the list.last is always second
-
-            // try recursive
-            return result;
+            }
+            return null;
         }
+
+        static int Recursive(int value)
+        {
+            if (value >= 10) return value;
+            value++;
+            //passes value back 5 times from the output of 5 different calls to Recursive
+            return Recursive(value);
+        }
+
+        //private List<int> DivideUp(int maxInt)
+        //{
+           
+
+            
+        //    result.Add(list[0]); // the list.first is always the first
+        //    result.Add(list[maxIndex]); // the list.last is always second
+
+        //    // try recursive
+        //    return result;
+        //}
 
         //public static void MergeSort(int[] input, int startIndex, int endIndex)
         //{
@@ -100,44 +112,44 @@ namespace DivideAndC
         //    return result;
         //}
 
-        [Fact]
-        public void OneToFive()
-        {
-            var result = DivideUp(7);
-            // if 1,2,3,4,5
-            Assert.Equal(new List<int> { 1, 5, 3, 4, 2 }, result);
-        }
+        //[Fact]
+        //public void OneToFive()
+        //{
+        //    var result = DivideUp(7);
+        //    // if 1,2,3,4,5
+        //    Assert.Equal(new List<int> { 1, 5, 3, 4, 2 }, result);
+        //}
 
-        [Fact]
-        public void OneToSeven()
-        {
-            var result = DivideUp(7);
-            // if 1,2,3,4,5,6,7 
-            Assert.Equal(new List<int> { 1, 7, 4, 5, 2, 6, 3 }, result);
-        }
+        //[Fact]
+        //public void OneToSeven()
+        //{
+        //    var result = DivideUp(7);
+        //    // if 1,2,3,4,5,6,7 
+        //    Assert.Equal(new List<int> { 1, 7, 4, 5, 2, 6, 3 }, result);
+        //}
 
-        [Fact]
-        public void OneToEight()
-        {
-            var result = DivideUp(8);
-            // if 1,2,3,4,5,6,7,8 
-            Assert.Equal(new List<int> { 1, 8, 4, 6, 2, 7, 5, 3 }, result);
-        }
+        //[Fact]
+        //public void OneToEight()
+        //{
+        //    var result = DivideUp(8);
+        //    // if 1,2,3,4,5,6,7,8 
+        //    Assert.Equal(new List<int> { 1, 8, 4, 6, 2, 7, 5, 3 }, result);
+        //}
 
-        [Fact]
-        public void OneToNine()
-        {
-            var result = DivideUp(9);
-            // if 1,2,3,4,5,6,7,8,9 
-            Assert.Equal(new List<int> { 1, 9, 5, 7, 3, 8, 6, 4, 2 }, result);
-        }
+        //[Fact]
+        //public void OneToNine()
+        //{
+        //    var result = DivideUp(9);
+        //    // if 1,2,3,4,5,6,7,8,9 
+        //    Assert.Equal(new List<int> { 1, 9, 5, 7, 3, 8, 6, 4, 2 }, result);
+        //}
 
-        [Fact]
-        public void OneToEleven()
-        {
-            var result = DivideUp(11);
-            // if 1,2,3,4,5,6,7,8,9,10,11
-            Assert.Equal(new List<int> { 1, 11, 6, 8, 3, 9, 7, 4, 2, 10, 5 }, result);
-        }
+        //[Fact]
+        //public void OneToEleven()
+        //{
+        //    var result = DivideUp(11);
+        //    // if 1,2,3,4,5,6,7,8,9,10,11
+        //    Assert.Equal(new List<int> { 1, 11, 6, 8, 3, 9, 7, 4, 2, 10, 5 }, result);
+        //}
     }
 }
