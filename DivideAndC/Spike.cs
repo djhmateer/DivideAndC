fc9b49afbd41c0aa32b2b9a41bf25ae396f98e01
Dave Mateer <davemateer@gmail.com>
2016-01-26 11:27:31 +0000

    got tests passing, however using a global scoped static list

--- DivideAndC/Spike.cs
+++ DivideAndC/Spike.cs
@@ -1,108 +1,111 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 namespace DivideAndC.Spike
 {
-    // lift and shift of Phils code
     public class Program
     {
         static void Main()
         {
-            int tar_dist = 8;
-            var n = new Node(1, tar_dist);
-            Console.WriteLine(n.left);
-            Console.WriteLine(n.right);
-            Queue nodelist = new Queue();
-            nodelist.Enqueue(n);
-            var result = div_conq(nodelist);
+            int max = 8;
+            var result = GetAnswer(max);
+            foreach (var number in result) Console.Write($"{number}, ");
         }
 
-        public static Queue div_conq(Queue nodelist)
+        public static List<int> GetAnswer(int max)
         {
-            if (nodelist.Count > 0)
+            // reset global _list - ahh!!
+            _list = new List<int>();
+            var node = new Node {Left = 1, Right = max};
+            _list.Add(node.Left);
+            _list.Add(node.Right);
+
+            var nodelist = new Queue<Node>();
+            nodelist.Enqueue(node);
+            DivConq(nodelist);
+            return _list;
+        }
+
+        static List<int> _list = new List<int>(); 
+
+        public static Queue<Node> DivConq(Queue<Node> nodeList)
+        {
+            if (nodeList.Count > 0)
             {
-                Node current_node = (Node)nodelist.Dequeue();
-                var midway = current_node.midway();
-                Console.WriteLine(midway);
+                Node currentNode = nodeList.Dequeue();
+                var midway = currentNode.Midway();
+                _list.Add(midway);
 
-                if (midway < current_node.right - 1)
+                if (midway < currentNode.Right - 1)
                 {
-                    var n = new Node(midway, current_node.right);
-                    nodelist.Enqueue(n);
+                    var n = new Node { Left = midway, Right = currentNode.Right };
+                    nodeList.Enqueue(n);
                 }
 
-                if (current_node.left < midway - 1)
+                if (currentNode.Left < midway - 1)
                 {
-                    var n = new Node(current_node.left, midway);
-                    nodelist.Enqueue(n);
+                    var n = new Node { Left = currentNode.Left, Right = midway };
+                    nodeList.Enqueue(n);
                 }
 
-                if (current_node.left < midway) return div_conq(nodelist);
-                if (midway < current_node.right) return div_conq(nodelist);
+                if (currentNode.Left < midway) return DivConq(nodeList);
+                if (midway < currentNode.Right) return DivConq(nodeList);
             }
-            // never gets here?
+            // done!
             return null;
         }
 
         public class Node
         {
-            public int left { get; set; }
-            public int right { get; set; }
+            public int Left { get; set; }
+            public int Right { get; set; }
 
-            public Node(int l, int r)
+            public int Midway()
             {
-                this.left = l;
-                this.right = r;
-            }
-
-            public int midway()
-            {
-                return (this.right - this.left) / 2 + this.left;
+                return (Right - Left) / 2 + Left;
             }
         }
 
 
-        //[Fact]
-        //public void OneToFive()
-        //{
-        //    var result = DivideUp(7);
-        //    // if 1,2,3,4,5
-        //    Assert.Equal(new List<int> { 1, 5, 3, 4, 2 }, result);
-        //}
+        [Fact]
+        public void OneToFive()
+        {
+            var result = GetAnswer(5);
+            // if 1,2,3,4,5
+            Assert.Equal(new List<int> { 1, 5, 3, 4, 2 }, result);
+        }
 
-        //[Fact]
-        //public void OneToSeven()
-        //{
-        //    var result = DivideUp(7);
-        //    // if 1,2,3,4,5,6,7 
-        //    Assert.Equal(new List<int> { 1, 7, 4, 5, 2, 6, 3 }, result);
-        //}
+        [Fact]
+        public void OneToSeven()
+        {
+            var result = GetAnswer(7);
+            // if 1,2,3,4,5,6,7 
+            Assert.Equal(new List<int> { 1, 7, 4, 5, 2, 6, 3 }, result);
+        }
 
-        //[Fact]
-        //public void OneToEight()
-        //{
-        //    var result = DivideUp(8);
-        //    // if 1,2,3,4,5,6,7,8 
-        //    Assert.Equal(new List<int> { 1, 8, 4, 6, 2, 7, 5, 3 }, result);
-        //}
+        [Fact]
+        public void OneToEight()
+        {
+            var result = GetAnswer(8);
+            // if 1,2,3,4,5,6,7,8 
+            Assert.Equal(new List<int> { 1, 8, 4, 6, 2, 7, 5, 3 }, result);
+        }
 
-        //[Fact]
-        //public void OneToNine()
-        //{
-        //    var result = DivideUp(9);
-        //    // if 1,2,3,4,5,6,7,8,9 
-        //    Assert.Equal(new List<int> { 1, 9, 5, 7, 3, 8, 6, 4, 2 }, result);
-        //}
+        [Fact]
+        public void OneToNine()
+        {
+            var result = GetAnswer(9);
+            // if 1,2,3,4,5,6,7,8,9 
+            Assert.Equal(new List<int> { 1, 9, 5, 7, 3, 8, 6, 4, 2 }, result);
+        }
 
-        //[Fact]
-        //public void OneToEleven()
-        //{
-        //    var result = DivideUp(11);
-        //    // if 1,2,3,4,5,6,7,8,9,10,11
-        //    Assert.Equal(new List<int> { 1, 11, 6, 8, 3, 9, 7, 4, 2, 10, 5 }, result);
-        //}
+        [Fact]
+        public void OneToEleven()
+        {
+            var result = GetAnswer(11);
+            // if 1,2,3,4,5,6,7,8,9,10,11
+            Assert.Equal(new List<int> { 1, 11, 6, 8, 3, 9, 7, 4, 2, 10, 5 }, result);
+        }
     }
 }
